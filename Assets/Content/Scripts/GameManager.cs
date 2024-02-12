8d22c80cfe13c140ee1628c431e7ae18c86ca4e3
AugustoCogollo <augusto.wyvern312@hotmail.com>
2024-02-12 11:22:07 -0600

    Spawnear grid acorde al objeto

--- Assets/Content/Scripts/GameManager.cs
+++ Assets/Content/Scripts/GameManager.cs
@@ -29,6 +29,9 @@ public class GameManager : MonoBehaviour
     [SerializeField]
     Material availableMat;
     Material obstacleMat;
+
+    [SerializeField] 
+    GenericEnemy[] enemies;
     
 
     void Start()
@@ -52,29 +55,48 @@ public class GameManager : MonoBehaviour
 
     void DecisionManager()
     {
-        if (player.lives <= 0)
-            currentState = GameState.GAMEOVER;
+        //if (player.lives <= 0)
+        //    currentState = GameState.GAMEOVER;
 
 
     }
 
+    public Vector3 GetGridCell(Vector2Int gridPosition)
+    {
+        foreach(GameObject cell in gridCells)
+        {
+            if(!TryGetComponent(out GridCell gridCell))
+            {
+                continue;
+            }
+
+            if (gridCell.gridID.Equals(gridPosition))
+            {
+                return gridCell.position;
+            }
+        }
+
+        return Vector3.zero;
+    }
+
     public void CreateGrid()
     {
-        for(float y = 0.5f; y < gridHeight; y++)
-            for(float x = 0.5f; x < gridWidth; x++)
+        for(float y = transform.position. z + 0.5f; y < (transform.position.z + gridHeight); y++)
+            for(float x = transform.position.x + 0.5f; x < (transform.position.x + gridWidth); x++)
             {
                 GameObject goCell = Instantiate(gridCellPrefab);
                 goCell.transform.localPosition = new Vector3(x, -1.4f, y);
                 goCell.transform.localScale = new Vector3(0.8f, 0.8f, 0.8f);
                 goCell.transform.localRotation = Quaternion.identity;
                 goCell.transform.SetParent(this.transform);
-                goCell.name = "Cell(" + x + "," + y + ")";
 
                 if(goCell.TryGetComponent(out GridCell gridCell))
                 {
                     gridCell.position = new Vector3(x, y, 0);
                     gridCell.type = GridType.WALKABLE;
+                    gridCell.gridID = new Vector2Int(((int)x), ((int)y));
                 }
+                goCell.name = "Cell(" + gridCell.gridID.x  + "," + gridCell.gridID.y + ")";
 
                 gridCells.Add(goCell);
             }
